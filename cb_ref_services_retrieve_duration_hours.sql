598e72cb5f58cb3c045d28ec5c57f2a58ea82b7d
small4ward1 <32926946+small4ward1@users.noreply.github.com>
2017-10-19 18:23:13 +0800

    Initial version of Retrieve duration hours SP

---
+++ cb_ref_services_retrieve_duration_hours.sql
@@ -0,0 +1,19 @@
+USE [iwantv4-sit]
+GO
+
+/****** Object:  StoredProcedure [dbo].[cb_ref_services_retrieve_duration_hours]    Script Date: 10/19/2017 4:09:49 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[cb_ref_services_retrieve_duration_hours]
+	@ServiceId INT
+AS
+BEGIN
+SELECT DURATIONHRS from CB_REF_SERVICES WITH (NOLOCK) WHERE ServiceId = @ServiceId
+END
+GO
+
+
