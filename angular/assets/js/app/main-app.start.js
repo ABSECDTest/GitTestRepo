7e53c71b2452366a64cbbddfe32fc0c10c8d6a96
ABSECDTest <32926946+ABSECDTest@users.noreply.github.com>
2017-11-08 12:26:39 +0800

    Release Additions and Deletions (#10)
    * Added text file in the packages folder.
    
    * Re-add text file. Wrong file added on previous commit.
    
    * Angular JS Tutorial code - Initial version with GET, POST, and DELETE.
    
    * deleted TestSite1 - not working with VS.
    
    * deleted remaining files in TestSite1

---
+++ angular/assets/js/app/main-app.start.js
@@ -0,0 +1,93 @@
+﻿"use strict";
+
+angular.module('MainApp', [
+])
+
+.controller("MainController", function ($scope, $http) {
+
+    //initialize scope variables
+    $scope.people = [];
+    $scope._name = "Default Name";
+    $scope._location = "Default Location";
+    $scope.user = {
+        name: function (theName) {
+            if (angular.isDefined(theName)) {
+                $scope._name = theName;
+            }
+            return $scope._name;
+        }(),
+        location: function (theLocation) {
+            if (angular.isDefined(theLocation)) {
+                $scope._location = theLocation;
+            }
+            return $scope._location;
+        }()
+    };
+
+    //initialize config for headers
+    var config = {
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'ZUMO-API-VERSION': '2.0.0'
+        },
+
+    };
+
+
+    //call getNames function
+    getNames();
+
+    function getNames() {
+        $http.get('https://test-evangelists-1.azurewebsites.net/tables/people', config)
+     .then(function (res) {
+         console.log(res);
+         $scope.people = res.data;
+     });
+    }
+
+    // add in resource 
+    function addName(user) {
+        alert("about to post!")
+        $http.post('https://test-evangelists-1.azurewebsites.net/tables/people', user, config)
+          .then(function (res) {
+              $scope.getNames();
+          });
+    }
+
+    function delName(user) {
+        var confirmres = confirm("You are about to delete this record. Action cannot be undone. Continue?");
+        var retrievedId = "";
+
+        if (confirmres == true) {
+            //get the ID via web service
+            $http.get('\\angular\\EvangelistsWebService.asmx/GetId', {
+                params: { name: user.name, location: user.location },
+                headers: { 'Access-Control-Allow-Origin': '*', 'ZUMO-API-VERSION': '2.0.0' },
+                dataType: "json",
+                contentType: "application/json; charset=utf-8"
+            })
+               .then(function (res) {
+                   $scope.retData = res.data;
+                   var obj = JSON.parse($scope.retData);
+                   angular.forEach(obj, function (item) {
+                       if (item.length == 0)
+                           alert('No data found');
+                       else {
+                           //perform delete after getting the ID and append it to url
+                           $http.delete('https://test-evangelists-1.azurewebsites.net/tables/people/' + item.id, config)
+                             .then(function (res) {
+                                 $scope.getNames();
+                             });
+                           alert(item.id + ' deleted');
+                       }
+                   });
+               });
+        }
+    }
+
+    $scope.addName = addName;
+    $scope.getNames = getNames;
+    $scope.delName = delName;
+
+})
+
